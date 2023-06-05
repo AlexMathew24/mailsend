befb4f176453b9e3ad8139b736a4f424fd3c640d
alex <alex2244mathew@gmail.com>
2023-06-05 20:07:44 +0530

    initial commit

---
+++ src/test/java/com/data/mailssend/MailssendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.mailssend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailssendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
